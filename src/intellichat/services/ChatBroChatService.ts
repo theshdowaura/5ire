b0c4c0c0f49583fbe397ea3c4e51c7cbd99a5a61
Ironben <hey@ironben.dev>
2025-04-23 11:43:04 +0800

    ♻️ (ChatBroChatService.ts): refactor API URL construction to use provider context for improved flexibility and maintainability 💡 (ChatBroChatService.ts): add eslint-disable comment for class-methods-use-this rule to clarify intentional design choice

--- src/intellichat/services/ChatBroChatService.ts
+++ src/intellichat/services/ChatBroChatService.ts
@@ -21,17 +21,18 @@ export default class ChatBroChatService
     this.provider = ChatBro;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   protected getReaderType() {
     return ChatBroReader;
   }
 
   protected async makePayload(
     messages: IChatRequestMessage[],
-    msgId?:string
+    msgId?: string,
   ): Promise<IChatRequestPayload> {
     const payload: IChatRequestPayload = {
       model: this.getModelName(),
-      messages: await this.makeMessages(messages,  msgId),
+      messages: await this.makeMessages(messages, msgId),
       temperature: this.context.getTemperature(),
       stream: true,
     };
@@ -44,32 +45,33 @@ export default class ChatBroChatService
 
   protected async makeRequest(
     messages: IChatRequestMessage[],
-    msgId?:string
+    msgId?: string,
   ): Promise<Response> {
     const payload = await this.makePayload(messages, msgId);
     debug('About to make a request, payload:\r\n', payload);
-    const { base, key } = this.apiSettings;
-    const url = urlJoin(`/v1/open/azure/chat`, base);
-    const postResp = await fetch(url, {
+    const provider = this.context.getProvider();
+    const postUrl = urlJoin(`/v1/open/azure/chat`, provider.apiBase.trim());
+    const postResp = await fetch(postUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-api-key': key,
+        'x-api-key': provider.apiKey.trim(),
       },
       body: JSON.stringify(payload),
     });
     const data: any = await postResp.json();
-    const response = await fetch(
-      `${base}/v1/open/azure/stream/chat/${data.key}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': key,
-        },
-        signal: this.abortController.signal,
-      },
+    const getUrl = urlJoin(
+      `/v1/open/azure/stream/chat/${data.key}`,
+      provider.apiBase.trim(),
     );
+    const response = await fetch(getUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': provider.apiKey.trim(),
+      },
+      signal: this.abortController.signal,
+    });
     return response;
   }
 }
