8afc7b89561d802c8433ea250981a46ab5b6cd44
Ironben <hey@ironben.dev>
2025-04-23 19:27:05 +0800

    💡 (Editor/index.tsx): adjust padding in tips container from px-5 to px-2.5 for better alignment and visual consistency

--- src/renderer/pages/chat/Editor/index.tsx
+++ src/renderer/pages/chat/Editor/index.tsx
@@ -177,7 +177,7 @@ export default function Editor({
       ) : null}
       <Toolbar onConfirm={onToolbarActionConfirm} isReady={isReady} />
       {!isReady && (
-        <div className="absolute top-[40px] right-0 left-0 z-10 tips flex justify-center flex-col items-center px-5">
+        <div className="absolute top-[40px] right-0 left-0 z-10 tips flex justify-center flex-col items-center px-2.5">
           <p>{t('Notification.APINotReady')}</p>
         </div>
       )}
