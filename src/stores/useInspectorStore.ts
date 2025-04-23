0ec5fc00cb8f239ae15ae9376ecba6ce516db400
Ironben <hey@ironben.dev>
2025-04-23 18:29:23 +0800

    ✨ (useInspectorStore.ts): add unique ID generation for trace messages using typeid-js ♻️ (useInspectorStore.ts): refactor trace method to include ID in message structure

--- src/stores/useInspectorStore.ts
+++ src/stores/useInspectorStore.ts
@@ -1,7 +1,8 @@
-import exp from 'constants';
+import { typeid } from 'typeid-js';
 import { create } from 'zustand';
 
 export interface ITraceMessage {
+  id: string;
   label: string;
   message: string;
 }
@@ -16,13 +17,14 @@ const useInspectorStore = create<IInspectorStore>((set, get) => ({
   messages: {},
   trace: (chatId: string, label: string, message: string) => {
     const { messages } = get();
+    const id = typeid('trace').toString();
     if (!messages[chatId]) {
-      set({ messages: { ...messages, [chatId]: [{ label, message }] } });
+      set({ messages: { ...messages, [chatId]: [{ id, label, message }] } });
     } else {
       set({
         messages: {
           ...messages,
-          [chatId]: messages[chatId].concat({ label, message }),
+          [chatId]: messages[chatId].concat([{ id, label, message }]),
         },
       });
     }
