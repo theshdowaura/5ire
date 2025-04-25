b0f8a53c3b0df7d85cfe4effbd8b83f934b3fd6e
Ironben <hey@ironben.dev>
2025-04-25 22:21:45 +0800

    🐛 (ToolStatusIndicator.tsx): fix typo in property name from `isBuiltin` to `isBuiltIn` to ensure correct functionality

--- src/renderer/components/ToolStatusIndicator.tsx
+++ src/renderer/components/ToolStatusIndicator.tsx
@@ -13,7 +13,7 @@ export default function ToolStatusIndicator(
   const { model, withTooltip, ...rest } = props;
 
   const originalSupport = useMemo(() => {
-    if (!model.isBuiltin || isNil(model.capabilities?.tools)) return false;
+    if (!model.isBuiltIn || isNil(model.capabilities?.tools)) return false;
     return true;
   }, [model.capabilities?.tools]);
 
